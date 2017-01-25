5d75216f97af2d4d05ea719bd392f1f64332042c
hoemoon <hoemoon.song@gmail.com>
2017-01-25 16:57:59 +0900

    social number checker done

---
+++ snippet/convertStringToArray.swift
@@ -0,0 +1,34 @@
+func convertToNumberArray(input: String) -> [Int] {
+    var intArray = [Int]()
+    
+    for i in input.characters {
+        let someString = String(i)
+        if let someInt = Int(someString) {
+            intArray += [someInt]
+        }
+    }
+    return intArray
+}
+
+func checkNumber(input: String) -> Bool {
+    let originalNumberArray = convertToNumberArray(input: input)
+    var convertalbeNumberArray = convertToNumberArray(input: input)
+    convertalbeNumberArray.remove(at: 0)
+    
+    let sevenNumber = originalNumberArray[12]
+    var numberN = 0
+    
+    print(originalNumberArray, convertalbeNumberArray)
+    
+    for index in 0...convertalbeNumberArray.count - 1 {
+        numberN += originalNumberArray[index] * convertalbeNumberArray[index]
+    }
+    print(numberN)
+    let k = (11 - (numberN % 10)) % 10
+    print("k", k)
+    if k == sevenNumber {
+        return true
+    } else {
+        return false
+    }
+}
--- snippet/request.swift
+++ snippet/request.swift
@@ -1,22 +1,10 @@
-//
-//  main.swift
-//  RandomNameMachine
-//
-//  Created by hoemoon on 24/01/2017.
-//  Copyright © 2017 hoemoon. All rights reserved.
-//
-
-import Foundation
-
-print("Hello")
-
 var shouldKeepRunning = true        // global
 
 URLSession(configuration: URLSessionConfiguration.default).dataTask(with: URL(string: "http://apple.com")!) { (data, response, error) in
     var resultHTML = String(data: data!, encoding: String.Encoding.utf8)
     print(resultHTML)
     shouldKeepRunning = false
-    }.resume()
+}.resume()
 
 while shouldKeepRunning && RunLoop.current.run(mode: .defaultRunLoopMode, before: .distantFuture) {
     

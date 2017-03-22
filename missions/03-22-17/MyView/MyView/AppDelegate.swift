eec51e8f3a64902f13386b5854ef56c66002df03
hoemoon <hoemoon.song@gmail.com>
2017-03-22 22:02:28 +0900

    add pacman fill done

--- missions/03-22-17/MyView/MyView/AppDelegate.swift
+++ missions/03-22-17/MyView/MyView/AppDelegate.swift
@@ -1,8 +1,8 @@
 //
 //  AppDelegate.swift
-//  MyVending
+//  MyView
 //
-//  Created by hoemoon on 20/03/2017.
+//  Created by hoemoon on 22/03/2017.
 //  Copyright © 2017 hoemoon. All rights reserved.
 //
 
@@ -13,66 +13,34 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        let vc = window?.rootViewController as! ViewController
-        // Unarchive machine
-        if let machine = UserDefaults.standard.object(forKey: "machine") as? Data {
-            if let unarch = NSKeyedUnarchiver.unarchiveObject(with: machine) as? VendingMachine {
-                vc.setMachine(machine: unarch)
-            } else {
-                vc.cokeCount.text = "0"
-                vc.cidarCount.text = "0"
-            }
-        }
-        
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-        print("applicationWillResignActive")
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-        
-        print("applicationDidEnterBackground")
-        // archive vendingmachine
-        let vc = window?.rootViewController as! ViewController
-        let machine = vc.getMachine()
-        let dateOfMachine:Data = NSKeyedArchiver.archivedData(withRootObject: machine)
-        UserDefaults.standard.set(dateOfMachine, forKey: "machine")
-        
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-        
-        print("applicationWillEnterForeground")
-        let vc = window?.rootViewController as! ViewController
-        // Unarchive machine
-        if let machine = UserDefaults.standard.object(forKey: "machine") as? Data {
-            if let unarch = NSKeyedUnarchiver.unarchiveObject(with: machine) as? VendingMachine {
-                vc.setMachine(machine: unarch)
-            }
-        }
-        
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-        // 들락날락 할때마다 실행된다.
-        // 처음켜면 실행 #2
-        print("applicationDidBecomeActive")
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-        print("applicationWillTerminatce")
-
     }
+
+
 }
 
---
+++ missions/03-22-17/MyView/MyView/MyView.swift
@@ -0,0 +1,86 @@
+//
+//  MyView.swift
+//  MyView
+//
+//  Created by hoemoon on 22/03/2017.
+//  Copyright © 2017 hoemoon. All rights reserved.
+//
+
+import UIKit
+
+class MyView: UIView {
+    override func draw(_ rect: CGRect) {
+        let context = UIGraphicsGetCurrentContext()
+        let gradi = CGGradient(colorsSpace: CGColorSpaceCreateDeviceRGB(), colors: [randomUIColor().cgColor, randomUIColor().cgColor] as CFArray, locations: nil)
+        
+        context?.drawLinearGradient(gradi!, start: CGPoint(x: self.bounds.midX, y: self.bounds.minY) , end: CGPoint(x: self.bounds.midX, y: self.bounds.maxY), options: CGGradientDrawingOptions.drawsBeforeStartLocation)
+        drawLine()
+        drawArc()
+    }
+    
+    func randomUIColor() -> UIColor {
+        let hue : CGFloat = CGFloat(arc4random() % 256) / 256 // use 256 to get full range from 0.0 to 1.0
+        let saturation : CGFloat = CGFloat(arc4random() % 128) / 256 + 0.5 // from 0.5 to 1.0 to stay away from white
+        let brightness : CGFloat = CGFloat(arc4random() % 128) / 256 + 0.5 // from 0.5 to 1.0 to stay away from black
+        return UIColor(hue: hue, saturation: saturation, brightness: brightness, alpha: 1)
+    }
+    
+    func randomFloat256() -> CGFloat {
+        return CGFloat(arc4random_uniform(255)) / 255
+    }
+    
+    func drawLine() {
+        for _ in 1...10 {
+            let myBezier = UIBezierPath()
+            myBezier.move(to: CGPoint(x: randomX(), y: randomY()))
+            myBezier.addLine(to: CGPoint(x: randomX(), y: randomY()))
+            
+            UIColor.black.setStroke()
+            myBezier.stroke()
+        }
+    }
+    
+    func drawArc() {
+        for _ in 1...10 {
+            let centerPoint = CGPoint(x: randomX(), y: randomY())
+            
+            let ciclePath: UIBezierPath = UIBezierPath(arcCenter: centerPoint, radius: CGFloat(randomX() / 2), startAngle: CGFloat((random360()).rad), endAngle: CGFloat((random360()).rad), clockwise: true)
+            ciclePath.addLine(to: centerPoint)
+            ciclePath.close()
+            ciclePath.stroke()
+            
+            let randomColor = randomUIColor().cgColor
+            let layer = CAShapeLayer()
+            layer.path = ciclePath.cgPath
+            layer.fillColor = randomColor
+            layer.backgroundColor = nil
+            layer.strokeColor = randomColor
+            
+            self.layer.addSublayer(layer)
+        }
+    }
+    
+    func random360() -> Int {
+        return Int(arc4random_uniform(360))
+    }
+    
+    func randomX() -> Int {
+        return Int(arc4random_uniform(UInt32(self.bounds.maxX)))
+    }
+    func randomY() -> Int {
+        return Int(arc4random_uniform(UInt32(self.bounds.maxY)))
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        guard let sublayers = self.layer.sublayers else { return }
+        for layer in sublayers {
+            layer.isHidden = true
+        }
+        self.setNeedsDisplay()
+    }
+}
+
+extension Int {
+    var rad: Double { return Double(self) * .pi / 180 }
+}
